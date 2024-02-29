df671ae6f270c6dd607b74e0dbfcc9b0809fde42
jhdcruz <jhdcruz@proton.me>
2024-03-01 00:34:04 +0800

    chore: initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,33 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    `gradle-enterprise`
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
+
+rootProject.name = "Goods"
+include(":app")
