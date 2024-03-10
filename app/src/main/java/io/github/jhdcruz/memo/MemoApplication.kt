d95c78517d333d8c89be91675a70be6bed5966ed
jhdcruz <jhdcruz@proton.me>
2024-03-11 00:30:35 +0800

    core: basic hilt module

---
+++ app/src/main/java/io/github/jhdcruz/memo/MemoApplication.kt
@@ -0,0 +1,7 @@
+package io.github.jhdcruz.memo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MemoApplication : Application()
---
+++ app/src/main/java/io/github/jhdcruz/memo/di/AppModule.kt
@@ -0,0 +1,34 @@
+package io.github.jhdcruz.memo.di
+
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.analytics
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
+import com.google.firebase.storage.storage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideFirestore() = Firebase.firestore
+
+    @Provides
+    @Singleton
+    fun provideAuth() = Firebase.auth
+
+    @Provides
+    @Singleton
+    fun provideStorage() = Firebase.storage
+
+    @Provides
+    @Singleton
+    fun provideAnalytics() = Firebase.analytics
+
+}
