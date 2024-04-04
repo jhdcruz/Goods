068eb73554589f3ecdb463e2b74bd6101e856595
jhdcruz <jhdcruz@proton.me>
2024-04-04 19:39:59 +0800

    fix(di): fix missing firebase module
    why this got ignored?????????

---
+++ app/src/main/java/io/github/jhdcruz/memo/di/FirebaseModule.kt
@@ -0,0 +1,39 @@
+package io.github.jhdcruz.memo.di
+
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.analytics
+import com.google.firebase.auth.auth
+import com.google.firebase.crashlytics.crashlytics
+import com.google.firebase.firestore.firestore
+import com.google.firebase.storage.storage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object FirebaseModule {
+
+    @Provides
+    @Singleton
+    fun provideFirestore() = Firebase.firestore
+
+    @Provides
+    @Singleton
+    fun provideAuth() = Firebase.auth
+
+    @Provides
+    @Singleton
+    fun provideStorage() = Firebase.storage
+
+    @Provides
+    @Singleton
+    fun provideAnalytics() = Firebase.analytics
+
+    @Provides
+    @Singleton
+    fun provideCrashlytics() = Firebase.crashlytics
+
+}
