617322b93a3452d9f39da611115c861cc1421797
jhdcruz <jhdcruz@proton.me>
2024-04-09 02:05:03 +0800

    fix(tasks): file uploads being overwritten

--- app/src/main/java/io/github/jhdcruz/memo/ui/tasks/details/AttachmentsList.kt
+++ app/src/main/java/io/github/jhdcruz/memo/ui/tasks/details/AttachmentsList.kt
@@ -3,6 +3,7 @@ package io.github.jhdcruz.memo.ui.tasks.details
 import android.net.Uri
 import android.provider.OpenableColumns
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
@@ -12,6 +13,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.InputChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -21,9 +24,10 @@ import io.github.jhdcruz.memo.ui.tasks.TasksViewModel
 fun AttachmentsList(tasksViewModel: TasksViewModel, attachments: List<Uri>?) {
     val context = LocalContext.current
     val fileProvider = context.contentResolver
+    val selectedAttachments = remember { mutableStateListOf<Pair<Int, String>>() }
 
     // Collect the display names of the attachments first
-    val attachmentNames = attachments?.mapIndexedNotNull { index, attachment ->
+    attachments?.mapIndexedNotNull { index, attachment ->
         fileProvider.query(
             attachment,
             arrayOf(OpenableColumns.DISPLAY_NAME),
@@ -32,7 +36,8 @@ fun AttachmentsList(tasksViewModel: TasksViewModel, attachments: List<Uri>?) {
         )?.use { cursor ->
             val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
             if (cursor.moveToFirst()) {
-                Pair(index, cursor.getString(nameIndex))
+                // append to selectedAttachments
+                selectedAttachments.add(index, Pair(index, cursor.getString(nameIndex)))
             } else {
                 null
             }
@@ -41,15 +46,20 @@ fun AttachmentsList(tasksViewModel: TasksViewModel, attachments: List<Uri>?) {
 
     LazyRow {
         // first = index, second = file name
-        attachmentNames.forEachIndexed { index, attachment ->
+        selectedAttachments.forEachIndexed { _, attachment ->
             item {
                 InputChip(
                     modifier = Modifier
-                        .width(120.dp)
+                        .padding(vertical = 8.dp, horizontal = 2.dp)
+                        .width(140.dp)
                         .height(32.dp),
                     selected = false,
                     label = {
-                        Text(text = attachment.second)
+                        // add ellipsis on text when going over max width
+                        Text(
+                            maxLines = 1,
+                            text = attachment.second
+                        )
                     },
                     trailingIcon = {
                         IconButton(
@@ -58,9 +68,7 @@ fun AttachmentsList(tasksViewModel: TasksViewModel, attachments: List<Uri>?) {
                                 tasksViewModel.removeTaskAttachment(attachment.first)
 
                                 // remove the attachment from attachmentNames
-                                attachmentNames.toMutableList().apply {
-                                    removeAt(index)
-                                }
+                                selectedAttachments.remove(attachment)
                             }) {
                             Icon(
                                 imageVector = Icons.Filled.Close,
