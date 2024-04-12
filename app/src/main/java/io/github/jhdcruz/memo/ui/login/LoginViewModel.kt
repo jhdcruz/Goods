42fb7005ae86f67a8e98de5f137c03f5135f2b22
jhdcruz <jhdcruz@proton.me>
2024-04-12 18:24:02 +0800

    chore: remove unused import

--- app/src/main/java/io/github/jhdcruz/memo/ui/login/LoginViewModel.kt
+++ app/src/main/java/io/github/jhdcruz/memo/ui/login/LoginViewModel.kt
@@ -1,9 +1,7 @@
 package io.github.jhdcruz.memo.ui.login
 
 import android.content.Context
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import io.github.jhdcruz.memo.domain.response.AuthResponseUseCase
 import kotlinx.coroutines.flow.Flow
 
 abstract class LoginViewModel : ViewModel() {
